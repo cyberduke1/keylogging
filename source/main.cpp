35cee7c0fabaeb92677534aa2d0fcfcdf372ef2d
luvozulucybertech1 <2567454@students.wits.ac.za>
2024-04-05 22:12:27 +0200

    fixed main

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,28 @@
-#include "keylog.cpp"
+#include "keylog.h"
 #include <ncurses.h>
 
 int main() {
     initscr();
     cbreak();
     noecho();
+    keypad(stdscr, TRUE); // Enable keypad for special keys like backspace
     refresh();
-    
+
     keylog M;
     while (true) {
         int ch = getch();
 
-        if (ch == 27)
+        if (ch == 27) // Exit when Escape key is pressed
             break;
-
-        M.Input(ch);
-        refresh();
+        else if (ch == KEY_BACKSPACE || ch == 127) { // Handle backspace
+            M.Input('\b'); // '\b' represents the backspace character
+            M.Input(' '); // Overwrite the character on the screen with a space
+            refresh();
+            move(getcury(stdscr), getcurx(stdscr) - 1); // Move cursor back one position
+        } else {
+            M.Input(ch); // Input other characters as usual
+            refresh();
+        }
     }
     endwin();
 
@@ -23,3 +30,4 @@ int main() {
 
     return 0;
 }
+
